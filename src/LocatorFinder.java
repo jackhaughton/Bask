ed1d3e5d957680b57d6471de01dd95449f462a1f
Jack Haughton <j.s.haughton.03@cantab.net>
2011-12-21 08:17:30 +0000

    Bit more work on locator finding

--- src/LocatorFinder.java
+++ src/LocatorFinder.java
@@ -13,7 +13,7 @@ public class LocatorFinder {
 	private static void findPotentialLocators(BinaryImage bi, ArrayList<Point> results, boolean horizontal) {
 		int w=bi.getWidth();
 		int h=bi.getHeight();
-		LinkedList<Integer> currentStack=new LinkedList<Integer>();
+		ArrayList<Integer> currentStack=new ArrayList<Integer>();
 		for (int a=0; a<(horizontal?h:w); a++) {
 			currentStack.clear();
 			boolean startColour=horizontal?bi.get(0,a):bi.get(a,0);
@@ -27,7 +27,20 @@ public class LocatorFinder {
 				}
 			}
 			currentStack.add((horizontal?w:h)-startPoint);
-			//TODO: now go through the stack looking for 1:1:3:1:1 patterns
+			
+			//now go through the stack looking for 1:1:3:1:1 patterns
+			final double MAX_ERR=0.1;
+			startPoint=currentStack.get(0);
+			for (int i=1; i<currentStack.size()-5; i++) {
+				double base=currentStack.get(i);
+				double err1=Math.abs((currentStack.get(i+1)/base)-1);
+				double err2=Math.abs((currentStack.get(i+2)/base)-3);
+				double err3=Math.abs((currentStack.get(i+3)/base)-1);
+				double err4=Math.abs((currentStack.get(i+4)/base)-1);
+				if (err1>MAX_ERR || err2>MAX_ERR || err3>MAX_ERR || err4>MAX_ERR) continue;
+				//If we get here, we've got a potential
+				//TODO: add it to the results list
+			}
 		}
 	}
 }
